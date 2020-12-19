0887ec7553f8e41507e4f88cb0f2b81192b6e635
Spaceific-Arch <praktisch@email.cz>
2020-12-19 23:48:27 +0100

    2020_12_19 delete __venv__

--- projects/install-module/pydroid-install.py
+++ projects/install-module/pydroid-install.py
@@ -1,5 +1,5 @@
 import pip
 import os
 
-pip.main(['install', 'pillow'])
+pip.main(['install', 'pipenv'])
 #os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip install --upgrade pip")
