f0fe991793d79c6005ac06dc33981b9b9497869a
praktisch <praktisch@email.cz>
2022-01-05 09:28:21 +0100

    2022_01_05-pydroid-install.py

---
+++ projects/csv/Assembly_code-from_CSV.py
@@ -0,0 +1,94 @@
+import os
+
+print("file {0}".format(__file__))
+
+myCwd = os.getcwd()
+print("my cwd {0}".format(myCwd))
+path = r"b:\\12-0168-0100_Modernizace_UVZ-3_stavba\\_Revit\\Koordinace\SNIM\\"
+savePath = r"b:\\12-0168-0100_Modernizace_UVZ-3_stavba\\_Revit\\Koordinace\SNIM\\"
+
+def ensure_dir(file_path):
+	directory = os.path.dirname(file_path) 
+	if not os.path.exists(directory):
+		os.makedirs(directory)		
+
+
+
+def transpose(inList):
+	returnList = [[inList[j][i] for j in range(len(inList))] for i in range(len(inList[0]))]
+	return returnList
+
+class Dic2obj(object):
+	def __init__(self, dictionary):
+		for key in dictionary:
+			setattr(self, key, dictionary[key])
+
+class InputData():
+	#FIRST_LINE = None
+	
+	def __init__(self, inPath, inSavePath):
+		""" if not InputData.FIRST_LINE:
+			InputData.FIRST_LINE = ["Region", "Confirmed", "Deads", "Recovered","D/C %", "C/Pop%", "c7D100k ", "d7D1m ", "incC8DStd%"] """
+		self.fileName = "2022_01_04-SNIM.txt"
+		self.saveFileName = "2022_01_04-SNIM-Assembly_code-windows-1250.txt"
+		self.savePath = inSavePath
+
+		self.path = os.path.join(inPath, self.fileName)
+
+		self.rawData = self.readFile(self.path)
+		#print("self.rawData len {0}\nself.dataDict {1}".format(self.rawData, self.dataDict['NS']))
+		for k,v in self.dataDict.items():
+			print("{0} - {1}\n".format(k,v))
+		
+		self.saveFile(self.savePath)
+	
+	def saveFile(self, inSavePath):
+		outputStr = ""
+		for k,v in self.dataDict.items():
+			outputStr += "{0}\t{1}\t{2}\t\n".format(k, v['Category Name'], 1)
+			for tIndex, typeName in v['Types'].items():
+				outputStr += "{0}{1}\t{2}\t{3} {4}\t\n".format(k, tIndex, v['Category Name'], typeName, 2)
+
+		ensure_dir(inSavePath)
+		#bStr = b"{0}".format()
+		#encodedStr = outputStr.encode("utf-8")
+		#encodedStr = outputStr.encode("latin-1")
+		sFile = open(os.path.join(inSavePath, self.saveFileName) , "w", encoding="windows-1250")
+		sFile.write(outputStr)
+		sFile.close()
+
+
+	def readFile(self, inPath):
+		with open(inPath) as f:
+			self.dataDict = {}
+			self.markDict = {}
+			i=0
+			previous = None
+			rawData = []
+			for line in f:
+				row = line.strip().split('\t')
+				if i > 1:
+					if row[0] == previous[0]:
+						if previous[2] not in self.markDict and previous[2] != "":
+							self.markDict[previous[2]] = previous[3]
+						elif previous[2] not in self.markDict and previous[2] == "":
+							self.markDict["01"] = "N/A"
+					else:
+						#print("previous[1] {0}".format(previous[1]))
+						self.dataDict[previous[0]] = {}
+						self.dataDict[previous[0]]['Category Name'] = previous[1]
+						self.dataDict[previous[0]]['Types'] = self.markDict
+						self.markDict = {}
+				else:
+					self.firstLine = row
+				previous = row
+				#print("{0} {1}".format(i, row))
+				rawData.append(row)
+				i +=1
+			f.close()
+
+		return rawData
+			
+
+
+inputJHU = InputData(path, savePath)
--- projects/install-module/pydroid-install.py
+++ projects/install-module/pydroid-install.py
@@ -5,7 +5,8 @@ import platform
 #os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip debug --verbose")
 
 
-#pip.main(['install', 'tensorflow'])
+#pip.main(['install', 'cupy'])
 #os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip install --upgrade pip")
 #os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip install --user tensorflow==2.2.0-rc4")
-os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip install --user xmltodict")
+#os.system(r"/data/user/0/ru.iiec.pydroid3/files/arm-linux-androideabi/bin/python3 -m pip install --user xmltodict")
+os.system(r"C:/Users/CZDAGE/AppData/Roaming/Python/Python38/Scripts/ipython3 -m pip install matplotlib")
