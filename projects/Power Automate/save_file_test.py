49a10fd0559b819f1bb64eb7f896f81446671753
Spaceific-Studio <praktisch@email.cz>
2022-06-10 10:05:21 +0200

    2022_06_10 save_file_test.py

---
+++ projects/Power Automate/save_file_test.py
@@ -0,0 +1,7 @@
+import os
+import time
+import random
+print(time.ctime())
+with open(r'c:/DANO/test.txt', mode='a') as file:
+	file.writelines("{0} - {1}\n".format(time.ctime(), random.randint(0,12)))
+	file.close()
