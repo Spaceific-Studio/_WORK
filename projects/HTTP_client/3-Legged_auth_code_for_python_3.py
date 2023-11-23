48b7a65ebc3910381108f9e2f2dc306ed9de8e98
Spaceific-Studio <praktisch@email.cz>
2023-11-23 11:46:29 +0100

    2023_11_23-3-Legged_auth_code_for_python_3.py update

--- projects/HTTP_client/3-Legged_auth_code_for_python_3.py
+++ projects/HTTP_client/3-Legged_auth_code_for_python_3.py
@@ -1,25 +1,56 @@
 import http.server
 import socketserver
 import webbrowser
+import urllib
 from urllib.parse import parse_qs, urlparse
 
 # Třída pro obsluhu požadavků s přidáním cesty
-print("http.server.SimpleHTTPRequestHandler dir({0}".format(dir(http.server.SimpleHTTPRequestHandler)))
+
+#print("http.server.SimpleHTTPRequestHandler dir({0}".format(dir(http.server.SimpleHTTPRequestHandler)))
+print("socketserver.TCPServer dir({0}".format(dir(socketserver.TCPServer)))
+class MyCode(object):
+    code = ""
+    def __init__(self):
+        pass
+    
+    @classmethod    
+    def setCode(cls, inText):
+        cls.code = inText
+        
+    @classmethod
+    def getCode(cls):
+        return cls.code
+    
+
+
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        print("self path: {0}".format(self.requestline))
-        self.path = self.requestline.split()[1]
-        super().do_GET()
+        # Získání adresy z TCPServer
+        server_host, server_port = self.server.server_address
+
+        # Zpracování zbytku kódu...
+        parsed_path = urllib.parse.urlparse(self.path)
+        print("parsed_path {0}".format(parsed_path))
+        
+        if "code" in parsed_path.query:
+            MyCode.setCode(parsed_path.query.split('=')[1])
+            #self.send_response(200)
+            #self.end_headers()
+            #response_text = f'Hello, this is a response from {server_host}:{server_port}'
+            #print(response_text)
+            #self.wfile.write(response_text.encode())
+        else:
+            super().do_GET()
     def getRequestLine():
         return self.requestline
 
 # Definujte funkci pro otevření okna s přihlašovacími údaji
+
 def get_authorization_code():
     # Nastavte potřebné proměnné
     client_id = 'Y5XrdAWZhwscrASWKXnARt2YCV0t3CHP'
     #redirect_uri = 'http://localhost:8080'
     redirect_uri = 'http%3A%2F%2Flocalhost%3A8080%2F'
-
     # Spusťte HTTP server na lokálním portu pro přesměrování URI
     with socketserver.TCPServer(("", 8080), MyHandler) as httpd:
         try:
@@ -28,10 +59,12 @@ def get_authorization_code():
 
             # Čekáme na přesměrování na naši URI a získáme autorizační kód
             #httpd.handle_request()  # Blocking call
+            #print(f"Serving on {httpd.server_address}")
             httpd.handle_request()
             #authorization_code = parse_qs(urlparse(httpd.path).query).get("code")
             #authorization_code = str(httpd.parse_request())
-            authorization_code = MyHandler.getRequestLine()
+            
+            authorization_code = ""
             print("authorization_code : {0}".format(authorization_code))
 
             # Zobrazíme autorizační kód
@@ -39,6 +72,6 @@ def get_authorization_code():
         except Exception as e:
             print(f"Chyba: {e}")
             return None
-
-OUT = get_authorization_code()
+get_authorization_code()
+OUT = MyCode.getCode()
 print('Authorization code: {0}'.format(OUT))
