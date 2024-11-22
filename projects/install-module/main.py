0dbe90af5cf2ff2cc184b0f61895c8b7a1d4dbb1
Spaceific-Studio <praktisch@email.cz>
2024-11-22 10:41:41 +0100

    2024_11_22 update.py

--- projects/install-module/main.py
+++ projects/install-module/main.py
@@ -5,6 +5,6 @@ import os
 #os.system(r"pip install tensorflow")
 #os.system(r"C:\Users\GercakD\AppData\Local\Programs\Python\Python310\Scripts -m pip install pyncclient")
 #print(dir(pip))
-pip.main(['install', 'python-docx'])
+pip.main(['install', 'cupy-cuda11x'])
 
 
